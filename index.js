235ef6583d1b845b699afa4b0b7b167c9385eb98
mustafairshaidd <mustafa.irshaid.113@gmail.com>
2023-08-31 03:37:34 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dbConnection from "./database/connection.js";
+
+import todolistsRouter from "./middleware/todolists/todolistsRouter.js";
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(todolistsRouter)
+
+app.use((error, req, res, next) => {
+  console.log(error.message)
+  res.status(error.statusCode).send({
+    status:false,
+    message:error.message,
+    stack:error.stack,
+    statusCode:error.statusCode,
+    errors:error.errors
+  })
+});
+
+app.listen(3000, () => {
+  console.log("server Sarting at http://localhost:3000");
+});
+
+dbConnection();
